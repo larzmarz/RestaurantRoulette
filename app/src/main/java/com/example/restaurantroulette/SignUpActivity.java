76f14e5302cdb9f7461b188bd4f1732ebd0c09e0
larzmarz <laurys3577@gmail.com>
2022-06-20 17:10:56 -0700

    final commit for day- signup

--- app/src/main/java/com/example/restaurantroulette/SignUpActivity.java
+++ app/src/main/java/com/example/restaurantroulette/SignUpActivity.java
@@ -3,12 +3,35 @@ package com.example.restaurantroulette;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseUser;
 
 public class SignUpActivity extends AppCompatActivity {
+    ParseUser user = new ParseUser();
+    public static final String TAG = "LoginActivity";
+    public EditText etUsernameSU;
+    public EditText etPasswordSU;
+    public EditText etEmailSU;
+    public EditText etFullName;
+
+    public Button btLoginSU;
+    public Button btSignupSU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        etUsernameSU = findViewById(R.id.etUsername);
+        etPasswordSU = findViewById(R.id.etPassword);
+        etEmailSU = findViewById(R.id.etEmailSU);
+        etFullName = findViewById(R.id.etFullName);
+        btLoginSU = findViewById(R.id.btLogin);
+        btSignupSU = findViewById(R.id.btSignup);
+        user.setUsername(etUsernameSU.getText().toString());
+        user.setPassword(etPasswordSU.getText().toString());
+        user.setEmail(etEmailSU.getText().toString());
+
     }
 }
