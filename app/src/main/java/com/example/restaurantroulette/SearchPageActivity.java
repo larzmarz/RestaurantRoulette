9fbdd09a176bc63019981c90703024ceddbe8c53
larzmarz <laurys3577@gmail.com>
2022-06-23 16:30:04 -0700

    final commit 4 day- worked on parse and establishing commands to get the yelp data

--- app/src/main/java/com/example/restaurantroulette/SearchPageActivity.java
+++ app/src/main/java/com/example/restaurantroulette/SearchPageActivity.java
@@ -4,7 +4,12 @@ import static com.facebook.stetho.inspector.network.PrettyPrinterDisplayType.JSO
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.RequestHeaders;
@@ -12,16 +17,43 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import org.json.JSONException;
 import org.json.JSONObject;
 import okhttp3.Headers;
-public class SearchPageActivity extends AppCompatActivity {
+public class SearchPageActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spTypeFood;
     private Spinner spPriceRange;
     private Spinner spLocation;
+    private Button btRandomize;
+
     //redirect to the specific api request I need
     public static final String BUSINESS_INFO = "https://api.yelp.com/v3/businesses/search";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+        spTypeFood = findViewById(R.id.spTypeFood);
+        spPriceRange = findViewById(R.id.spPriceRange);
+        spLocation = findViewById(R.id.spMileRadius);
+        btRandomize = findViewById(R.id.btRandomize);
+        btRandomize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SearchPageActivity.this, "Random Button Works", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //setting the spinners
+        String[] typeFood = getResources().getStringArray(R.array.food_items);
+        String[] priceRange = getResources().getStringArray(R.array.price_range);
+        String[] mileRadius = getResources().getStringArray(R.array.mile_radius);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, typeFood);
+        ArrayAdapter adapter1 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, priceRange);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_item, mileRadius);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spTypeFood.setAdapter(adapter);
+        spPriceRange.setAdapter(adapter1);
+        spLocation.setAdapter(adapter2);
+
+        //calling Yelp API commands
         AsyncHttpClient client = new AsyncHttpClient();
         RequestHeaders headers = new RequestHeaders();
         //TODO: Hide API Key
@@ -41,5 +73,17 @@ public class SearchPageActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {}
         });
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
--- app/src/main/java/com/example/restaurantroulette/fragment/HomeFragment.java
+++ app/src/main/java/com/example/restaurantroulette/fragment/HomeFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.restaurantroulette.R;
@@ -18,18 +19,30 @@ import com.example.restaurantroulette.SearchPageActivity;
 
 public class HomeFragment extends Fragment implements View.OnClickListener{
     public Button btStart;
+    public EditText etZipCode;
+    //TODO: change to int when time to set limits on zip codes
+    public String zipCode;
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_home, container, false);
+        etZipCode = view.findViewById(R.id.etZipCode);
+        //zipCode = Integer.parseInt(etZipCode.getText().toString());
         btStart = view.findViewById(R.id.btStartSearch);
         //when the user clicks the start button
         btStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "redirect successful", Toast.LENGTH_SHORT).show();
-                goSearchPage();
+                //TODO: Set limits on zip codes so they don't exceed 5 digits, note: some zipcodes start with two zeros
+                if(!etZipCode.getText().toString().isEmpty()){
+                    Toast.makeText(getContext(), "Let the adventure begin", Toast.LENGTH_SHORT).show();
+                    //this zip code is what will go into the yelp location section
+                    zipCode = etZipCode.getText().toString();
+                    goSearchPage();
+                }else{
+                    Toast.makeText(getContext(), "Enter your Zip Code", Toast.LENGTH_SHORT).show();
+                }
             }
         });
         return view;
